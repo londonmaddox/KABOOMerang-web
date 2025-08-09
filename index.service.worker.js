a0b2d1848869b39a6099c575e9af395022c59683
londonmaddox <londonmaddox13@gmail.com>
2025-08-09 19:40:44 +1200

    Version 1.1

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754280328|653180056';
+const CACHE_VERSION = '1754725190|848697725';
 /** @type {string} */
 const CACHE_PREFIX = 'KABOOMerang-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
