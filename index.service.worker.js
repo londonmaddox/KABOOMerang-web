3a2ea82897158f32062e5d726f44d94f142bd9fc
londonmaddox <londonmaddox13@gmail.com>
2025-08-21 20:13:16 +1200

    VERSION 1.2.2
    Fixed lag problems

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755632530|5456418775';
+const CACHE_VERSION = '1755763971|2969693382';
 /** @type {string} */
 const CACHE_PREFIX = 'KABOOMerang-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
