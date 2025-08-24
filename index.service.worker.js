ea55a545e028a395f965ea9ca10a3af9e1acc569
londonmaddox <londonmaddox13@gmail.com>
2025-08-24 15:38:16 +1200

    VERSION 1.3.2
    Added a door that closed when you start the game and added a cap for the camera zoom

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755983253|758673454';
+const CACHE_VERSION = '1756006622|7055874876';
 /** @type {string} */
 const CACHE_PREFIX = 'KABOOMerang-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
